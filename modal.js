8c1900cb8049b800d478545730ad5bcb64aa7aec
kimikoi123 <kimuelanqui3@gmail.com>
2022-03-12 14:42:05 +0800

    march 12 edit

--- modal.js
+++ modal.js
@@ -1,11 +1,16 @@
+import { format } from "date-fns"
+
 const modalBody = document.querySelector('[data-modal-body]')
 const modal = document.querySelector('[data-modal]')
 
 
 
 const eventFormTemplate = document.getElementById('event-form-template')
-export function openModal(date) {
+export function openModal(event) {
     const eventFormElement = eventFormTemplate.content.cloneNode(true)
+    const isNewEvent = event.id == null
+    eventFormElement.querySelector('[data-event-title]').textContent = isNewEvent ? 'Add Event' : 'Edit Event'
+    eventFormElement.querySelector('[data-date]').textContent = format(event, 'L    /d/yyyy')
     modalBody.append(eventFormElement)
     modal.classList.add('show')
 }
